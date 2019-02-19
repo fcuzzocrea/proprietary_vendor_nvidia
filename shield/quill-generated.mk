7cb725918b7357bbec000c115388dd3ad10f5aea
Aaron Kling <webgeek1234@gmail.com>
2019-02-18 22:55:59 -0600

    Update quill makefiles

--- shield/quill-generated.mk
+++ shield/quill-generated.mk
@@ -13,12 +13,29 @@
 # limitations under the License.
 
 PRODUCT_PACKAGES += \
+                    vendor.nvidia.hardware.graphics.composer@1.0-service \
+                    vendor.nvidia.hardware.graphics.composer@1.0 \
+                    vendor.nvidia.hardware.graphics.composer@1.0-impl \
+                    libnvhwcomposer \
+                    libnvblit \
+                    libnvddk_vic \
+                    libnvos \
+                    libnvrm \
+                    libnvcolorutil \
+                    libnvrm_graphics \
+                    libnvgr \
+                    libnvrm_gpu \
+                    libnvdc \
+                    libnvimp \
+                    vendor.nvidia.hardware.graphics.display@1.0 \
+                    libtlk_secure_hdcp_up \
+                    libtsechdcp \
+                    vendor.nvidia.hardware.graphics.display@1.0-impl \
                     audio.primary.tegra_32 \
                     libnvos_32 \
                     libnvoice_32 \
                     libnvaudiofx_32 \
                     gatekeeper.trusty.tegra_32 \
-                    libtrusty_32 \
                     gralloc.tegra_32 \
                     libnvgr_32 \
                     libnvrm_32 \
@@ -28,29 +45,10 @@ PRODUCT_PACKAGES += \
                     libnvddk_vic_32 \
                     libnvrm_gpu_32 \
                     hdmi_cec.tegra_32 \
-                    hwcomposer.tegra_32 \
-                    libnvhwc_service_32 \
-                    libtlk_secure_hdcp_up_32 \
-                    libtsechdcp_32 \
                     vulkan.tegra_32 \
                     hdmi_cec.tegra \
                     gralloc.tegra \
-                    libnvgr \
-                    libnvos \
-                    libnvrm \
-                    libnvcolorutil \
-                    libnvrm_graphics \
-                    libnvblit \
-                    libnvddk_vic \
-                    libnvrm_gpu \
-                    hwcomposer.tegra \
-                    libnvhwc_service \
-                    libnvdc \
-                    libnvimp \
-                    libtlk_secure_hdcp_up \
-                    libtsechdcp \
                     gatekeeper.trusty.tegra \
-                    libtrusty \
                     keystore.v0.tegra \
                     keystore.v1.tegra \
                     vulkan.tegra \
@@ -67,28 +65,26 @@ PRODUCT_PACKAGES += \
                     libnvrmapi_tegra_32 \
                     libnvcpl_32 \
                     libnvphs_32 \
+                    vendor.nvidia.hardware.phs@1.0_32 \
                     libGLESv1_CM_tegra_32 \
                     libglcore_32 \
+                    libnvrmvkif_32 \
+                    libnvidia-glvkspirv_32 \
                     libnvdc_32 \
                     libnvimp_32 \
                     libGLESv2_tegra_32 \
-                    libnvRSDriver_32 \
-                    libcuda_32 \
-                    libnvidia-fatbinaryloader_32 \
-                    libnvRSCompiler_32 \
                     libnvwsi_32 \
                     libEGL_tegra \
                     libnvglsi \
                     libnvrmapi_tegra \
                     libnvcpl \
                     libnvphs \
+                    vendor.nvidia.hardware.phs@1.0 \
                     libGLESv1_CM_tegra \
                     libglcore \
+                    libnvrmvkif \
+                    libnvidia-glvkspirv \
                     libGLESv2_tegra \
-                    libnvRSDriver \
-                    libcuda \
-                    libnvidia-fatbinaryloader \
-                    libnvRSCompiler \
                     libnvwsi \
                     libnvmm_audio_32 \
                     libnvmm_utils_32 \
@@ -111,7 +107,9 @@ PRODUCT_PACKAGES += \
                     libnvomxadaptor_32 \
                     libnvomxilclient_32 \
                     libcuda_32 \
+                    libnvidia-fatbinaryloader_32 \
                     libcuda \
+                    libnvidia-fatbinaryloader \
                     pbc2 \
                     pbc.tegra \
                     pbc.tegra_32
