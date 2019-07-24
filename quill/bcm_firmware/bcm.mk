0ba21b99d4bf873410d70d9cb94669b3e0adf67c
Aaron Kling <webgeek1234@gmail.com>
2019-07-24 09:04:07 -0500

    quill: add bcm firmware, clm, and nvram
    It uses 4354

--- quill/bcm_firmware/bcm.mk
+++ quill/bcm_firmware/bcm.mk
@@ -12,7 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, vendor/nvidia/t186/t186.mk)
-$(call inherit-product, vendor/nvidia/common/common-by-flags.mk)
-$(call inherit-product, vendor/nvidia/quill/bcm_firmware/bcm.mk)
-$(call inherit-product, vendor/nvidia/shield/shield-by-flags.mk)
+$(call inherit-product, vendor/nvidia/common/bcm_firmware/bcm4354/device-bcm.mk)
+$(call inherit-product, $(LOCAL_PATH)/clm.mk)
+$(call inherit-product, $(LOCAL_PATH)/nvram.mk)
+
+PRODUCT_PACKAGES += \
+                    bcm4350 \
+                    fw_bcmdhd_4354
