18ac3474da4f5ed5a1b3ce08ddfb7c6a02d3f33a
Aaron Kling <webgeek1234@gmail.com>
2020-03-11 10:34:08 -0500

    Build clearkey 1.2

--- common/widevine/widevine.mk
+++ common/widevine/widevine.mk
@@ -15,7 +15,7 @@
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.1-service.clearkey \
+    android.hardware.drm@1.2-service.clearkey \
     android.hardware.drm@1.1-service.widevine
 
 PRODUCT_PROPERTY_OVERRIDES += \
